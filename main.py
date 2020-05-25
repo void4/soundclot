c7f21f3a7b3f1088126a6add6ed17ef1ab68b825
void4 <arndtschnabel@freenet.de>
2020-05-25 14:31:52 +0000

    Yellow

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, redirect, request, render_template
+import flask_monitoringdashboard as dashboard
 
 app = Flask(__name__)
+
+dashboard.config.init_from(file="config.cfg")
+dashboard.bind(app)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -11,4 +15,3 @@ PORT = 8000
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=PORT)
-
