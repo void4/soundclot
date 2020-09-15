f9f61c1468c58ed86e9dcf7bce350904d969fb52
void4 <arndtschnabel@freenet.de>
2020-09-15 11:23:54 +0200

    Don't require email for additions anymore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, url_for
 import flask_monitoringdashboard as dashboard
 from random import shuffle
+import os
 
 app = Flask(__name__)
 
@@ -12,8 +13,19 @@ dashboard.bind(app)
 def index(path):
 	with open("videos.txt") as f:
 		videos = f.read().splitlines()
-	shuffle(videos)	
+	shuffle(videos)
 	return render_template("index.html", videos=videos)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+	#os.makedirs("feedback", exist_ok=True)
+	data = request.form["link"]
+
+	with open("feedback.txt", "a") as f:
+		f.write(data+"\n")
+
+	return redirect(url_for("index"))
+
 
 PORT = 8000
 
